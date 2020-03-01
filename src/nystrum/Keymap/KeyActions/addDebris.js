953e8663c1b00defa97e901d56b30eb4a84e7b82
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2020-02-29 21:54:30 -0500

    jarods debris code

--- src/nystrum/Keymap/KeyActions/addDebris.js
+++ src/nystrum/Keymap/KeyActions/addDebris.js
@@ -1,37 +1,40 @@
 import * as Helper from '../../../helper';
+import * as Constant from '../../constants';
 import { Debris } from '../../entites';
 import * as Item from '../../items';
 
 export const addDebris = (game) => {
   
+  let pos = null
 
-  let pos = Helper.getRandomPos(game.map).coordinates
+  for (var i = 20; i > 0; i--) {
+    var box = new Debris({
+      pos,
+      renderer: {
+        character: '%',
+        color: Constant.THEMES.SOLARIZED.base2,
+        background: Constant.THEMES.SOLARIZED.base01,
+      },
+      name: 'Box',
+      game,
+      durability: 10,
+    })
+    game.randomlyPlaceActorOnMap(box)
+  }
 
-  let box = new Debris({
-    pos,
-    renderer: {
-      character: 'B',
-      color: 'brown',
-      background: 'brown',
-    },
-    name: 'Box',
-    game,
-    durability: 10,
-  })
-
-  let stick = new Debris({
-    pos,
-    renderer: {
-      character: '/',
-      color: 'black',
-      background: '',
-    },
-    name: 'stick',
-    game,
-    durability: 5,
-  })
-
-  if (game.randomlyPlaceActorOnMap(box) && game.randomlyPlaceActorOnMap(stick)) {
-    game.draw();
-  };
+  for (var i = 20; i > 0; i--) {
+    var stick = new Debris({
+      pos,
+      renderer: {
+        character: 'L',
+        color: Constant.THEMES.SOLARIZED.base2,
+        background: Constant.THEMES.SOLARIZED.base01,
+      },
+      name: 'stick',
+      game,
+      durability: 5,
+    })
+    game.randomlyPlaceActorOnMap(stick)
+  }
+  game.draw();
 }
